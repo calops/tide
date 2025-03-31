149de8f784e37dcc71d9734b19bbe2e32c41124d
Rémi Labeyrie <calops@tocards.net>
2025-04-01 01:28:19 +0200

    pleaase

--- functions/_tide_item_nix_shell.fish
+++ functions/_tide_item_nix_shell.fish
@@ -1,11 +1,12 @@
 function _tide_item_nix_shell
     set -q IN_NIX_SHELL || return
 
+    set -l purity_icon
     if test "$IN_NIX_SHELL" = impure
-        set -l purity_icon ' '
+        set purity_icon ' '
     else
-        set -l purity_icon ' '
+        set purity_icon ' '
     end
 
-    _tide_print_item nix_shell $tide_nix_shell_icon $purity_icon $name
+    _tide_print_item nix_shell $tide_nix_shell_icon' ' $purity_icon $name
 end
