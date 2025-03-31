852f0138d83fe31a4c4ca561bfcddb9153fda005
Rémi Labeyrie <calops@tocards.net>
2025-03-31 22:19:37 +0200

    test

--- functions/_tide_item_nix_shell.fish
+++ functions/_tide_item_nix_shell.fish
@@ -1,3 +1,4 @@
 function _tide_item_nix_shell
-    set -q IN_NIX_SHELL && _tide_print_item nix_shell $tide_nix_shell_icon' ' $IN_NIX_SHELL
+    # set -q IN_NIX_SHELL && _tide_print_item nix_shell $tide_nix_shell_icon' ' $IN_NIX_SHELL
+    _tide_print_item nix_shell TOTO
 end
